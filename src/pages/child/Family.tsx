3cf3b3ac6fb5fabaeb302c3ac5ef8c6c4694f5af
maxpowernz <aaronguilford@outlook.com>
2020-11-08 12:21:19 +1300

    commit

--- src/pages/child/Family.tsx
+++ src/pages/child/Family.tsx
@@ -1,8 +1,23 @@
+import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { useAppState } from "../../AppStateContext";
 
 export default function Family() {
   const { state, dispatch } = useAppState();
 
-  return <div>{state.selectedChild.family.father.firstName}</div>;
+  const { father, mother, careGiver } = state.selectedChild.family;
+
+  return (
+    <Typography>
+      <div>
+        Mother: {mother.firstName} {mother.lastName}
+      </div>
+      <div>
+        Father: {father.firstName} {father.lastName}
+      </div>
+      <div>
+        Caregiver: {careGiver.firstName} {careGiver.lastName}
+      </div>
+    </Typography>
+  );
 }
